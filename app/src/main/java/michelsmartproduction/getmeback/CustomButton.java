48d34c995b49b84af6d9ba7a76bfc095dca796fc
Lucas <lucas.beck94@gmail.com>
2017-01-06 18:09:41 +0100

    Design page d'accueil + ebauche navigation

---
+++ app/src/main/java/michelsmartproduction/getmeback/CustomButton.java
@@ -0,0 +1,47 @@
+package michelsmartproduction.getmeback;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.Button;
+
+/**
+ * Created by Lucas on 06/01/2017.
+ */
+public class CustomButton extends Button {
+
+    public CustomButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        //Typeface.createFromAsset doesn't work in the layout editor. Skipping...
+        if (isInEditMode()) {
+            return;
+        }
+
+        //accède aux attributs ajoutés à cette CustomTextView
+        TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.CustomTextView);
+
+        //récupère l'attribut "font"
+        String fontName = styledAttrs.getString(R.styleable.CustomTextView_font);
+
+        //permet au garbage collector de récupérer l'espace utilisé par ce TypedArray
+        styledAttrs.recycle();
+
+        //puis modifie la font de cet élément
+        setTypeFace(fontName);
+    }
+
+    public void setTypeFace(String fontName) {
+        if(fontName != null){
+            try {
+                Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontName);
+                setTypeface(typeface);
+            } catch (Exception e) {
+                Log.e("FONT", fontName + " not found", e);
+            }
+        }
+    }
+
+}
--- app/src/main/java/michelsmartproduction/getmeback/MainActivity.java
+++ app/src/main/java/michelsmartproduction/getmeback/MainActivity.java
@@ -1,14 +1,33 @@
 package michelsmartproduction.getmeback;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-//coucou Michel
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        // Pour cacher la status barre
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+
+        final Button getBackButton = (Button) findViewById(R.id.getBack);
+        assert getBackButton != null;
+        getBackButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, test.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
