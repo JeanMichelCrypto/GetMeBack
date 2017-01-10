48a2b3f9fa719a20e86ae6c0b9881213f9bf3462
Romain <nicolai.romain12@gmail.com>
2017-01-10 07:28:42 +0100

    ajout de la géolocalisation fonctionnelle

--- app/src/main/java/michelsmartproduction/getmeback/MainActivity.java
+++ app/src/main/java/michelsmartproduction/getmeback/MainActivity.java
@@ -8,9 +8,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity  {
+
+
+    private static final int PERMISSION_ACCESS_COARSE_LOCATION = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSION_ACCESS_COARSE_LOCATION);
+        }
+
         FragmentManager fragmentManager = getFragmentManager();
         android.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         Menu Menufragment = new Menu();
@@ -27,4 +45,11 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
 
     }
+
+
+
+
+
+
+
 }
--- app/src/main/java/michelsmartproduction/getmeback/Menu.java
+++ app/src/main/java/michelsmartproduction/getmeback/Menu.java
@@ -9,17 +9,57 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by Lucas on 07/01/2017.
  */
-public class Menu extends Fragment {
+public class Menu extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     AlertDialog.Builder dlgAlert ;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int PERMISSION_ACCESS_COARSE_LOCATION = 1;
+    public String lat = "";
+    public String lon ="";
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.menu, container, false);
         //Instancier vos composants graphique ici (faîtes vos findViewById)
+
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSION_ACCESS_COARSE_LOCATION);
+        }
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
 
         dlgAlert  = new AlertDialog.Builder(getActivity());
         final Button listButton = (Button) view.findViewById(R.id.listButton);
@@ -44,12 +84,60 @@ public class Menu extends Fragment {
 
             @Override
             public void onClick(View v) {
-                dlgAlert.setMessage("This is an alert with no consequence");
-                dlgAlert.setTitle("App Title");
-                dlgAlert.setCancelable(true);
-                dlgAlert.create().show();
+                if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                            mGoogleApiClient);
+                    if (mLastLocation != null) {
+                        lat = String.valueOf(mLastLocation.getLatitude());
+                        lon = String.valueOf(mLastLocation.getLongitude());
+                    }
+                    dlgAlert.setMessage("Lat= " + lat + " & long= " + lon);
+                    dlgAlert.setTitle("Position récupérée");
+                    dlgAlert.setCancelable(true);
+                    dlgAlert.create().show();
+
+                }
             }
         });
 
         return view; }
+
+    public void onConnected(@Nullable Bundle bundle) { //bundle=(connectionHint)
+        Log.i(MainActivity.class.getSimpleName(), "Connected to Google Play Services!");
+
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+           // Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    //mGoogleApiClient);
+           // if (mLastLocation != null) {
+                // mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+                // mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+               // lat = String.valueOf(mLastLocation.getLatitude());
+               // lon = String.valueOf(mLastLocation.getLongitude());
+
+            }
+        }
+
+    //}
+    public void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    public void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
