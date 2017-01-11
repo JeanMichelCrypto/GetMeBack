18537ab9b7706172db9e45a64f62cc6b941e7305
Romain <nicolai.romain12@gmail.com>
2017-01-11 16:24:47 +0100

    ajout de MAJ de localisation

--- app/src/main/java/michelsmartproduction/getmeback/Menu.java
+++ app/src/main/java/michelsmartproduction/getmeback/Menu.java
@@ -1,10 +1,12 @@
 package michelsmartproduction.getmeback;
 
+import android.Manifest;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,32 +14,39 @@ import android.widget.Button;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+
+import java.util.Date;
+
 
 /**
  * Created by Lucas on 07/01/2017.
  */
-public class Menu extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class Menu extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-    AlertDialog.Builder dlgAlert ;
+    AlertDialog.Builder dlgAlert;
     private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mLastLocation;
+
+
+    private long INTERVAL = 10 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
     private static final int PERMISSION_ACCESS_COARSE_LOCATION = 1;
     public String lat = "";
-    public String lon ="";
+    public String lon = "";
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.menu, container, false);
@@ -48,6 +57,7 @@ public class Menu extends Fragment implements GoogleApiClient.ConnectionCallback
             ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
                     PERMISSION_ACCESS_COARSE_LOCATION);
         }
+        createLocationRequest();
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
                     .addConnectionCallbacks(this)
@@ -55,18 +65,15 @@ public class Menu extends Fragment implements GoogleApiClient.ConnectionCallback
                     .addApi(LocationServices.API)
                     .build();
         }
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-
-        dlgAlert  = new AlertDialog.Builder(getActivity());
+        // mGoogleApiClient.connect(); //--------------------------------------------------------------------------------------------
+        dlgAlert = new AlertDialog.Builder(getActivity());
         final Button listButton = (Button) view.findViewById(R.id.listButton);
         final Button posButton = (Button) view.findViewById(R.id.savePos);
 
         assert listButton != null;
         assert posButton != null;
+
 
         listButton.setOnClickListener(new View.OnClickListener() {
 
@@ -82,44 +89,43 @@ public class Menu extends Fragment implements GoogleApiClient.ConnectionCallback
 
         posButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                            mGoogleApiClient);
-                    if (mLastLocation != null) {
-                        lat = String.valueOf(mLastLocation.getLatitude());
-                        lon = String.valueOf(mLastLocation.getLongitude());
-                    }
-                    dlgAlert.setMessage("Lat= " + lat + " & long= " + lon);
-                    dlgAlert.setTitle("Position récupérée");
-                    dlgAlert.setCancelable(true);
-                    dlgAlert.create().show();
+                                         @Override
+                                         public void onClick(View v) {
+                                             if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                                                     == PackageManager.PERMISSION_GRANTED) {
 
-                }
-            }
-        });
 
-        return view; }
+                                             }
+                                             startLocationUpdates();
+                                             Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                                                     mGoogleApiClient);
+                                             if (mLastLocation != null) {
+                                                 lat = String.valueOf(mLastLocation.getLatitude());
+                                                 lon = String.valueOf(mLastLocation.getLongitude());
+                                             }
+                                             dlgAlert.setMessage("Lat= " + lat + " & long= " + lon);
+                                             dlgAlert.setTitle("Position récupérée");
+                                             dlgAlert.setCancelable(true);
+                                             dlgAlert.create().show();
+
+                                         }
+                                     }
+        );
+
+        return view;
+    }
 
     public void onConnected(@Nullable Bundle bundle) { //bundle=(connectionHint)
-        Log.i(MainActivity.class.getSimpleName(), "Connected to Google Play Services!");
+        //Log.i(MainActivity.class.getSimpleName(), "Connected to Google Play Services!");
 
-        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+       /* if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-           // Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    //mGoogleApiClient);
-           // if (mLastLocation != null) {
-                // mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-                // mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
-               // lat = String.valueOf(mLastLocation.getLatitude());
-               // lon = String.valueOf(mLastLocation.getLongitude());
 
-            }
-        }
+        }*/
 
-    //}
+    }
+
+
     public void onStart() {
         super.onStart();
         if (mGoogleApiClient != null) {
@@ -131,13 +137,37 @@ public class Menu extends Fragment implements GoogleApiClient.ConnectionCallback
         mGoogleApiClient.disconnect();
         super.onStop();
     }
+
     @Override
     public void onConnectionSuspended(int i) {
-
+        mGoogleApiClient.connect();
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+    private void createLocationRequest() {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+
+    }
+
+    protected void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSION_ACCESS_COARSE_LOCATION);
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
 }
